4f02fa6778db8bf6b4019afeeab90121609d06c6
Akeree <akeredayo@gmail.com>
2019-06-24 12:26:11 +0100

    git commit -m

--- experiment.js
+++ experiment.js
@@ -1,35 +1,35 @@
-let section_a = document.getElementById('section-a').scrollHeight;
-console.log(section_a);
+// let section_a = document.getElementById('section-a').scrollHeight;
+// console.log(section_a);
 
-let section_b = document.getElementById('section-b').scrollHeight;
-console.log(section_b);
+// let section_b = document.getElementById('section-b').scrollHeight;
+// console.log(section_b);
 
-let section_c = document.getElementById('section-c').scrollHeight;
-console.log(section_c);
+// let section_c = document.getElementById('section-c').scrollHeight;
+// console.log(section_c);
 
-let floating_text = document.getElementById('floating-text');
-let floating_plus1 = document.getElementById('floating-plus1');
-let floating_plus2 = document.getElementById('floating-plus2');
+// let floating_text = document.getElementById('floating-text');
+// let floating_plus1 = document.getElementById('floating-plus1');
+// let floating_plus2 = document.getElementById('floating-plus2');
 
-console.log(window.scrollY);
-console.log(floating_plus1);
-if(window.scrollY > section_a ){
-    console.log('tes');
-    floating_text.style.color = "white";
-    floating_plus1.style.backgroundColor = "white";
-    floating_plus2.style.backgroundColor = "white";
-}
-else if(window.scrollY >= (section_a + section_b)){
-    floating_text.style.color = "black";
-    floating_plus1.style.backgroundColor = "black";
-    floating_plus2.style.backgroundColor = "black";
-}
-else{
-    floating_text.style.color = "white";
-    floating_plus1.style.backgroundColor = "white";
-    floating_plus2.style.backgroundColor = "white";
+// console.log(window.scrollY);
+// console.log(floating_plus1);
+// if(window.scrollY > section_a ){
+//     console.log('tes');
+//     floating_text.style.color = "white";
+//     floating_plus1.style.backgroundColor = "white";
+//     floating_plus2.style.backgroundColor = "white";
+// }
+// else if(window.scrollY >= (section_a + section_b)){
+//     floating_text.style.color = "black";
+//     floating_plus1.style.backgroundColor = "black";
+//     floating_plus2.style.backgroundColor = "black";
+// }
+// else{
+//     floating_text.style.color = "white";
+//     floating_plus1.style.backgroundColor = "white";
+//     floating_plus2.style.backgroundColor = "white";
 
-}
+// }
     
 
 
