95e668cc34ba70e2d4c4ac5940a889fbf1edb0b8
Akeree <akeredayo@gmail.com>
2019-07-10 14:46:16 +0100

    journal

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+function greet(firstName,lastName,horific,greeting)){
+    return `${greeting}${horific}${lastName}${firstName}! I'm extremy pleased you can join us, `
+
+}
+console.log(greet("Brian","holt","lord","salutation"))
