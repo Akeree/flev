72f0abd7762eafd2453823296b99eaa0cc242b65
Akeree <akeredayo@gmail.com>
2019-09-03 07:39:20 +0100

    flev

--- script.js
+++
@@ -1,7 +0,0 @@
-
-
-function greet(firstName,lastName,horific,greeting)){
-    return `${greeting}${horific}${lastName}${firstName}! I'm extremy pleased you can join us, `
-
-}
-console.log(greet("Brian","holt","lord","salutation"))
